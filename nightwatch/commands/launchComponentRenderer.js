760696dea240f7142d3b9bf95108365995bc744b
Vaibhav Singh <vaibhav.s@browserstack.com>
2022-09-28 18:32:26 +0530

    initial commit

---
+++ nightwatch/commands/launchComponentRenderer.js
@@ -0,0 +1,2 @@
+// Workaround until plugins can include other plugins
+module.exports = require('vite-plugin-nightwatch/nightwatch/commands/launchComponentRenderer.js');
---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import vue from '@vitejs/plugin-vue'
+import nightwatchPlugin from 'vite-plugin-nightwatch'
+
+export default {
+  optimizeDeps: {
+    include: ['vue']
+  },
+  plugins: [
+    vue(),
+    nightwatchPlugin({
+      componentType: 'vue'
+    })
+  ]
+};
