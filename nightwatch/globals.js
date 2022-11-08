a4e62d355b76c1331813cec791048de9b3fd6a18
Ravi Sawlani <ravisawlani04@gmail.com>
2022-11-08 19:12:21 +0530

    refactor; added errors with help code

--- nightwatch/globals.js
+++ nightwatch/globals.js
@@ -1,35 +1,111 @@
-const fs = require('fs');
-const path = require('path');
-
+const http = require('../lib/setup.js');
 const setup = require('../lib/setup.js');
 
-let viteServer;
-const projectConfigFile = path.join(process.cwd(), 'vite.config.js');
-const viteConfigFile = path.join(__dirname, '../vite.config.js');
 
-const hasProjectConfigFile = () => {
-  try {
-    return fs.statSync(projectConfigFile).isFile();
-  } catch (err) {
-    return false;
+let viteServer;
+const isWorker = process.argv.includes('--test-worker');
+const startViteServer = async function(settings = {}) {
+  settings.vite_dev_server = Object.assign({
+    start_vite: true,
+    port: 5173
+  }, settings.vite_dev_server || {});
+
+  let vitePort;
+  if(settings.vite_dev_server.start_vite) {
+    await setup();
+
+    settings.vite_dev_server.port = vitePort = viteServer.config.server.port;
+  } else {
+    vitePort = settings.vite_dev_server.port; 
+    try {
+      const enabled = await makeViteRequest(vitePort);
+      if(!enabled) {
+        const error = new Error('Missing vite-plugin-nightwatch');
+        const code = `:
+
+    import nightwatchPlugin from 'vite-plugin-nightwatch'
+
+    export default {
+      plugins: [
+        // ... other plugins
+        nightwatchPlugin({
+          componentType: 'vue'
+        })
+      ]
+    };
+    `;
+        error.help = ['Please ensure that "vite-plugin-nightwatch" is loaded in your Vite config file ' + code];
+        throw error;
+      }
+
+      return true;
+    } catch(err) {
+      const error = new Error('vite dev server is not running: \n  ' + err.message);
+      error.help = [`You can configure Nightwatch to start Vite automatically by adding this to your nightwatch.conf.js: 
+      vite_dev_server: {
+        start_vite: true,
+        port: 5173 
+      }
+      `];
+      throw error;
+    } 
+  }
+
+  this.launchUrl = this.baseUrl = `http://localhost:${vitePort}`;
+};
+
+const stopViteServer = async () => {
+  if(viteServer) {
+    await viteServer.close();
+    viteServer = null;
   }
 }
 
 module.exports = {
-  async before(settings) {
-    viteServer = await setup({
-      viteConfigFile: hasProjectConfigFile() ? projectConfigFile : viteConfigFile
-    });
-
-    // This will make sure the launch Url is set correctly when mounting the React component
-    const vite_port = viteServer.config.server.port;
-    settings.vite_port = vite_port;
-    this.launchUrl = this.baseUrl = `http://localhost:${vite_port}`;
+  async beforeEach() {
+    if(isWorker) {
+      await startViteServer.call(this, settings);
+    }
   },
 
+  async before(settings) {
+    if(!settings.parallel_mode && !settings.testWorkersEnabled) {
+      await startViteServer.call(this, settings);
+    }
+  },
+
+
   async after() {
-    if (viteServer) {
-      await viteServer.close();
+    await stopViteServer();
+  },
+
+  async afterEach() {
+    if(isWorker) {
+      await stopViteServer();
     }
   }
+}
+
+function makeViteRequest(port) {
+  const options = {
+    host: 'localhost',
+    port,
+    path: '/_nightwatch'
+  };
+
+  return new Promise((resolve, reject) => {
+    const req = http.request(options, function(response) {
+      if (response.statusCode === 404) {
+        return resolve(false);
+      }
+
+      resolve(true);
+    });
+
+    req.on('error', err => {
+      reject(err);
+    });
+
+    req.end();
+  });
 }
