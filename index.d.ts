d0ae9f86841c24e3799b9d3a27fadb3f4eae9332
Vaibhav Singh <vaibhav.s@browserstack.com>
2023-04-21 12:39:16 +0530

    Add: type declaration file, update dependencies (#3)

---
+++ index.d.ts
@@ -0,0 +1,24 @@
+import { NightwatchAPI, Element } from 'nightwatch';
+import { MountingOptions } from '@vue/test-utils';
+
+type GlobalMountOptions = NonNullable<MountingOptions<any>['global']>;
+
+declare module 'nightwatch' {
+  interface NightwatchAPI {
+    importScript(
+      scriptPath: string,
+      options: { scriptType: string; componentType: string },
+      callback?: () => void
+    ): this;
+    mountComponent(
+      componentPath: string,
+      options?: {
+        props?: Pick<MountingOptions<any>, 'props'>;
+        plugin?: Pick<GlobalMountOptions, 'plugins'>;
+        mocks?: Pick<GlobalMountOptions, 'mocks'>;
+      },
+      callback?: (this: NightwatchAPI, result: Element) => void
+    ): Awaitable<this, Element>;
+    launchComponentRenderer(): this;
+  }
+}
