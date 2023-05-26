c251399e625b8cf24bac5467b8e9b6601c5f62a9
AutomatedTester <david.burns@theautomatedtester.co.uk>
2023-05-26 19:11:04 +0100

    update conf to use selenium manager

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -39,7 +39,7 @@ module.exports = {
 
       webdriver: {
         start_process: true,
-        server_path: require('chromedriver').path,
+        server_path: '',
         cli_args: [
           // --verbose
         ]
